59adbcd633bbf9ac5d2cd0e0cfa13d848fdbe773
Tetiana <tetiana@you-agency.com>
2024-01-17 21:57:52 +0000

    delete unused functions

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -63,31 +63,3 @@ export const getIdFromLocalCookie = () => {
 export const getTokenFromLocalCookie = () => {
   return Cookies.get('jwt');
 };
-
-export const getTokenFromServerCookie = (req: NextRequest) => {
-  if (!req.headers.cookie || '') {
-    return undefined;
-  }
-  const jwtCookie = req.headers.cookie
-    .split(';')
-    .find((c) => c.trim().startsWith('jwt='));
-  if (!jwtCookie) {
-    return undefined;
-  }
-  const jwt = jwtCookie.split('=')[1];
-  return jwt;
-};
-
-export const getIdFromServerCookie = (req) => {
-  if (!req.headers.cookie || '') {
-    return undefined;
-  }
-  const idCookie = req.headers.cookie
-    .split(';')
-    .find((c) => c.trim().startsWith('id='));
-  if (!idCookie) {
-    return undefined;
-  }
-  const id = idCookie.split('=')[1];
-  return id;
-};
